a8cc888f49ea118c25ec2c8bb2ca48a5cbd29656
Mark Weston <mark.weston@pactcoffee.com>
2019-03-19 17:43:01 +0000

    refactoring Sphere to combine transforms into a single matrix

--- lib/acts_as_raytracer/sphere.rb
+++ lib/acts_as_raytracer/sphere.rb
@@ -1,13 +1,24 @@
 class Sphere
   def initialize(colour: Colour.new(red: 0, green: 0, blue: 0))
-    @position = Point.new(0, 0, 0)
-    @radius = 1
     @colour = colour
-    @transforms = [Transform::Identity.new]
+    @transforms = []
   end
 
-  attr_reader :position, :radius, :colour
-  attr_accessor :transforms
+  private def object_space_origin
+    return Point.new(0, 0, 0)
+  end
+
+  private def unit_radius_squared
+    return 1
+  end
+
+  attr_reader :colour, :transforms
+
+  def combined_transform
+    return @combined_transform ||= transforms.reverse.reduce(Transform::Identity.new) do |combined, transform|
+      combined * transform
+    end
+  end
 
   def scale(x:, y:, z:)
     self.transforms << Transform::Scaling.new(x: x, y: y, z: z)
@@ -20,13 +31,11 @@ class Sphere
   end
 
   def intersect(ray:)
-    object_space_ray = self.transforms.reduce(ray) do |new_ray, transform|
-      new_ray.transform(matrix: transform.inverse)
-    end
-    sphere_to_ray = object_space_ray.origin.to_v - position.to_v
-    a = Vector.dot_product(object_space_ray.direction, object_space_ray.direction)
-    b = 2 * Vector.dot_product(object_space_ray.direction, sphere_to_ray)
-    c = Vector.dot_product(sphere_to_ray, sphere_to_ray) - radius**2
+    ray_in_object_space = ray.transform(matrix: combined_transform.inverse)
+    sphere_to_ray = ray_in_object_space.origin.to_v - object_space_origin.to_v
+    a = Vector.dot_product(ray_in_object_space.direction, ray_in_object_space.direction)
+    b = 2 * Vector.dot_product(ray_in_object_space.direction, sphere_to_ray)
+    c = Vector.dot_product(sphere_to_ray, sphere_to_ray) - unit_radius_squared
     discriminant = b**2 - 4 * a * c
     return nil if discriminant < 0
 
@@ -36,13 +45,9 @@ class Sphere
   end
 
   def normal_at(intersect:)
-    object_space_intersect = self.transforms.reduce(intersect) do |new_intersect, transform|
-      transform.inverse * new_intersect
-    end
-    object_space_normal = object_space_intersect.to_v
-    world_space_normal = self.transforms.reduce(object_space_normal) do |normal, transform|
-      transform.inverse.transpose * normal
-    end
+    intersect_in_object_space = combined_transform.inverse * intersect
+    object_space_normal = intersect_in_object_space.to_v
+    world_space_normal = combined_transform.inverse.transpose * object_space_normal
     return world_space_normal.normalise!
   end
 
--- raytrace.rb
+++ raytrace.rb
@@ -3,11 +3,11 @@ require_relative 'lib/acts_as_raytracer'
 world = World.new(objects: [
     Plane.new(colour: Colour.new(red: 0.8, green: 0.7, blue: 0.7)),
     Sphere.new(colour: Colour.new(red: 1, green: 0, blue: 0))
-          .translate(x: 1.5, y: 0.5, z: 4)
-          .scale(x: 1.5, y: 0.2, z: 1.5),
+          .scale(x: 1.5, y: 0.2, z: 1.5)
+          .translate(x: 1.5, y: 0.5, z: 4),
     Sphere.new(colour: Colour.new(red: 0, green: 1, blue: 0))
-          .translate(x: -0.2, y: 0.3, z: 1)
-          .scale(x: 0.25, y: 0.4, z: 0.25),
+          .scale(x: 0.25, y: 0.4, z: 0.25)
+          .translate(x: -0.2, y: 0.3, z: 1),
     ],
     light: LightSource.new(position: Point.new(8, 3, -5)))
 
